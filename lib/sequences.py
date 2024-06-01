f2214d1bedaebab83e7b2a03e5a390331e1962bd
lidyaghilay <lidya.ghilay@student.moringaschool.com>
2024-06-01 22:09:31 +0300

    Done

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,17 @@
-#!/usr/bin/env python3
-
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        return []
+    elif length == 1:
+        return [0]
+    elif length == 2:
+        return [0, 1]
+    else:
+        first, second = 0, 1
+        fib_sequence = []
+        for i in range(length):
+            fib_sequence.append(first)
+            first, second = second, first + second
+        return fib_sequence
+
+# Test the function
+print(print_fibonacci(10))
